5320c67e320f573a86161e3859957f8d188ea399
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2025-08-19 10:28:57 -0400

    added search hero on search main page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,26 +6,36 @@ import { PAGE_EVENTS_SEARCH } from '@/app/_data/constants';
 import withPageTracking from '@/app/_hocs/withPageTracking';
 import QuestionsAnswers from '@/app/_widgets/QuestionsAnswers';
 import SearchResults from '@/app/_widgets/SearchResults';
+import BasicSearch from "@/app/_widgets/BasicSearch"
 
 const Search = (): JSX.Element => {
   const searchParams = useSearchParams();
   const query = searchParams?.get('q') || '';
 
   return (
-    <div className="m-auto pt-30 min-h-screen bg-gray-50">
-      <div className="max-w-7xl mx-auto px-4 py-8">
-        <div className="mb-6">
-          <p className="text-gray-800 text-lg w-full text-left">Showing results for <strong className="text-blue-700 text-700">{`"${query}"`}</strong></p>
+    <>
+      <div className="bg-blue-700 flex justify-center py-20 px-4 bg-gradient-to-r from-blue-600 to-blue-800">
+        <div className="w-[800px] text-center">
+          <h1 className="text-4xl md:text-6xl font-bold mb-8 text-white">SkyWings Search</h1>
+          <BasicSearch rfkId="rfkid_6" />
         </div>
-        <QuestionsAnswers
-          key={`${query}-questions`}
-          rfkId="rfkid_qa"
-          defaultKeyphrase={query}
-          defaultRelatedQuestions={4}
-        />
-        <SearchResults key={`${query}-search`} rfkId="rfkid_7" defaultKeyphrase={query} />
       </div>
-    </div>
+      <div className="m-auto pt-10 min-h-screen bg-gray-50">
+        <div className="max-w-7xl mx-auto px-4 py-8">
+          <div className="mb-6">
+            <p className="text-gray-800 text-lg w-full text-left">Showing results for <strong className="text-blue-700 text-700">{`"${query}"`}</strong></p>
+          </div>
+          <QuestionsAnswers
+            key={`${query}-questions`}
+            rfkId="rfkid_qa"
+            defaultKeyphrase={query}
+            defaultRelatedQuestions={4}
+          />
+          <SearchResults key={`${query}-search`} rfkId="rfkid_7" defaultKeyphrase={query} />
+        </div>
+      </div>
+    </>
+
   );
 };
 
